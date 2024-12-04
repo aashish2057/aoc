63ada245aee947fd418f11889589b2b5e987c843
Aashish Sharma <aashish2057@gmail.com>
2024-12-04 15:05:02 -0600

    nuke repo

--- y2024/main.go
+++ y2024/main.go
@@ -2,16 +2,9 @@ package main
 
 import (
     "fmt"
-    "github.com/aashish2057/aoc/y2024/challenges/day1"
-    "github.com/aashish2057/aoc/y2024/challenges/day2"
 )
 
 func main() {
     fmt.Println("---    Advent of Code 2024    ---")
     fmt.Println("--- Day 1: Historian Hysteria ---")
-    fmt.Println("Part 1: ", day1.Part1())
-    fmt.Println("Part 2: ", day1.Part2())
-    fmt.Println("--- Day 2: Red-Nosed Reports  ---")
-    fmt.Println("Part 1: ", day2.Part1())
-    fmt.Println("Part 2: ", day2.Part2())
 }
